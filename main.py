d2d07dcbac5327387b2e6e379d7242ee1d8b335a
Future-Tens <zxc40612zxc@gmail.com>
2026-06-20 22:23:53 +0800

    [ 26/06/20 ] Merge dev/restruct and usiong Release token.

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def run_bot():
     with open(config_path, 'r', encoding='utf-8') as f:
         config = json.load(f)
     
-    if not (token := config.get("test_token")):
+    if not (token := config.get("token")):
         print("錯誤：config.json 內的 token 為空或未設定。")
         return
 
